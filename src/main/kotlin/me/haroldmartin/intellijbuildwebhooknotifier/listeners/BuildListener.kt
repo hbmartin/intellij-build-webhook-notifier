158b6b0c92ce832a0c67abb6f4b0031ac5b0ec5e
Harold Martin <harold.martin@gmail.com>
2022-11-07 08:35:14 -0800

    replace intellij component with listener

--- src/main/kotlin/me/haroldmartin/intellijbuildwebhooknotifier/listeners/BuildListener.kt
+++ src/main/kotlin/me/haroldmartin/intellijbuildwebhooknotifier/listeners/BuildListener.kt
@@ -1,14 +1,5 @@
 package me.haroldmartin.intellijbuildwebhooknotifier.listeners
 
-import com.intellij.execution.ExecutionListener
-import com.intellij.execution.ExecutionManager
-import com.intellij.execution.process.ProcessHandler
-import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.openapi.compiler.CompilationStatusListener
-import com.intellij.openapi.compiler.CompileContext
-import com.intellij.openapi.compiler.CompilerTopics
-import com.intellij.openapi.components.BaseComponent
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.task.ProjectTaskContext
 import com.intellij.task.ProjectTaskListener
@@ -16,58 +7,21 @@ import com.intellij.task.ProjectTaskManager
 import me.haroldmartin.intellijbuildwebhooknotifier.ServiceLocator
 import me.haroldmartin.intellijbuildwebhooknotifier.model.BuildStatus
 
-class BuildListener(project: Project) : BaseComponent {
+class BuildListener(private val project: Project) : ProjectTaskListener {
     private val buildNotifier = ServiceLocator.buildNotifier
-    private val logger: Logger = ServiceLocator.logger
 
-    private val compilationStatusListener = object : CompilationStatusListener {
-        override fun compilationFinished(aborted: Boolean, errors: Int, warnings: Int, compileContext: CompileContext) {
-            logger.warn("compilationStatusListener: compilationFinished, $aborted, $errors, $compileContext")
-            buildNotifier(
-                buildStatus = when {
-                    errors > 0 -> BuildStatus.ERROR
-                    aborted -> BuildStatus.CANCELLED
-                    else -> BuildStatus.SUCCESS
-                },
-                projectName = project.name
-            )
-        }
+    override fun finished(result: ProjectTaskManager.Result) {
+        buildNotifier(
+            buildStatus = when {
+                result.hasErrors() -> BuildStatus.ERROR
+                result.isAborted -> BuildStatus.CANCELLED
+                else -> BuildStatus.SUCCESS
+            },
+            projectName = project.name
+        )
     }
 
-    private val executionListener = object : ExecutionListener {
-        override fun processStartScheduled(executorId: String, env: ExecutionEnvironment) {
-            logger.warn("processStartScheduled: $env")
-            buildNotifier(
-                buildStatus = BuildStatus.STARTED,
-                projectName = project.name
-            )
-        }
-
-        override fun processTerminated(
-            executorId: String,
-            env: ExecutionEnvironment,
-            handler: ProcessHandler,
-            exitCode: Int
-        ) {
-            logger.warn("processTerminated: $env, $exitCode")
-        }
-    }
-
-    private val projectTaskListener = object : ProjectTaskListener {
-        override fun finished(result: ProjectTaskManager.Result) {
-            logger.warn("projectTaskListener: finished: $result")
-        }
-
-        override fun started(context: ProjectTaskContext) {
-            logger.warn("projectTaskListener: started")
-        }
-    }
-
-    init {
-        val messages = project.messageBus.connect()
-
-        messages.subscribe(CompilerTopics.COMPILATION_STATUS, compilationStatusListener)
-        messages.subscribe(ExecutionManager.EXECUTION_TOPIC, executionListener)
-        messages.subscribe(ProjectTaskListener.TOPIC, projectTaskListener)
+    override fun started(context: ProjectTaskContext) {
+        buildNotifier(BuildStatus.STARTED, project.name)
     }
 }

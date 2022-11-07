fed2396a95dff4f93cd32259b8bf12b08bed49b5
Harold Martin <harold.martin@gmail.com>
2022-11-07 08:27:10 -0800

    replace android component with listener

--- src/main/kotlin/me/haroldmartin/intellijbuildwebhooknotifier/listeners/AndroidBuildListener.kt
+++ src/main/kotlin/me/haroldmartin/intellijbuildwebhooknotifier/listeners/AndroidBuildListener.kt
@@ -2,48 +2,33 @@ package me.haroldmartin.intellijbuildwebhooknotifier.listeners
 
 import com.android.tools.idea.gradle.project.build.BuildContext
 import com.android.tools.idea.gradle.project.build.GradleBuildListener
-import com.android.tools.idea.gradle.project.build.GradleBuildState
 import com.android.tools.idea.gradle.project.build.invoker.GradleBuildInvoker
-import com.intellij.openapi.components.BaseComponent
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import me.haroldmartin.intellijbuildwebhooknotifier.ServiceLocator
 import me.haroldmartin.intellijbuildwebhooknotifier.model.BuildStatus
 import com.android.tools.idea.gradle.project.build.BuildStatus as GradleBuildStatus
 import me.haroldmartin.intellijbuildwebhooknotifier.model.BuildStatus as WhStatus
 
-class AndroidBuildListener(project: Project) : BaseComponent {
+class AndroidBuildListener(private val project: Project) : GradleBuildListener {
     private val buildNotifier = ServiceLocator.buildNotifier
-    private val logger: Logger = ServiceLocator.logger
 
-    init {
-        GradleBuildState.subscribe(
-            project,
-            object : GradleBuildListener {
-                override fun buildExecutorCreated(request: GradleBuildInvoker.Request) {
-                    logger.warn("buildExecutorCreated")
-                }
+    override fun buildExecutorCreated(request: GradleBuildInvoker.Request) = Unit
 
-                override fun buildStarted(context: BuildContext) {
-                    logger.warn("buildStarted")
-                    buildNotifier(BuildStatus.STARTED, project.name)
-                }
+    override fun buildStarted(context: BuildContext) {
+        buildNotifier(BuildStatus.STARTED, project.name)
+    }
 
-                override fun buildFinished(
-                    status: GradleBuildStatus,
-                    context: BuildContext?
-                ) {
-                    logger.warn("buildFinished: $status")
-                    buildNotifier(
-                        buildStatus = when (status) {
-                            GradleBuildStatus.SUCCESS -> WhStatus.SUCCESS
-                            GradleBuildStatus.CANCELED -> WhStatus.CANCELLED
-                            GradleBuildStatus.FAILED -> WhStatus.ERROR
-                        },
-                        projectName = project.name
-                    )
-                }
-            }
+    override fun buildFinished(
+        status: GradleBuildStatus,
+        context: BuildContext?
+    ) {
+        buildNotifier(
+            buildStatus = when (status) {
+                GradleBuildStatus.SUCCESS -> WhStatus.SUCCESS
+                GradleBuildStatus.CANCELED -> WhStatus.CANCELLED
+                GradleBuildStatus.FAILED -> WhStatus.ERROR
+            },
+            projectName = project.name
         )
     }
 }
